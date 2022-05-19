d50170ebb40341d5acf64c54557b7603427e5695
Toshimichi0915 <main@toshimichi.net>
2022-05-19 19:50:40 +0900

    Add get/post

---
+++ index.js
@@ -0,0 +1,42 @@
+import AWS from "aws-sdk"
+
+const db = new AWS.DynamoDB.DocumentClient()
+
+async function postDocument(id, body, contentType) {
+  return await db.put({
+    TableName: "documents",
+    TimeToLive: 3600,
+    Item: { id, body, contentType }
+  }).promise()
+}
+
+async function getDocument(id) {
+  return await db.get({
+    TableName: "documents",
+    Key: { id }
+  }).promise()
+}
+
+export async function get(event) {
+
+  console.debug(event)
+  const document = (await getDocument(event.queryStringParameters.q)).Item
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": document.contentType
+    },
+    body: document.body
+  }
+}
+
+export async function post(event) {
+
+  console.debug(event)
+  postDocument(event.queryStringParameters.q, event.body, event.headers["Content-Type"] ?? "raw")
+
+  return {
+    statusCode: 200
+  }
+}
